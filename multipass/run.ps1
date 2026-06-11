81733ee6a716ca075345283189f0cd1c2489d345
Christopher Schuf <christopherschuf@MBPvonCistopher.fritz.box>
2026-06-11 09:36:32 +0200

    changed multipass script after troubleshoot

--- multipass/run.ps1
+++ multipass/run.ps1
@@ -1,34 +1,54 @@
-#start vms
-echo "Starting VMs"
-multipass launch --cloud-init controller-cloud-config.yml --disk 5G --mem 1G --cpus 1 --name controller-1
-multipass launch --cloud-init controller-cloud-config.yml --disk 5G --mem 1G --cpus 1 --name controller-2
-multipass launch --cloud-init controller-cloud-config.yml --disk 5G --mem 1G --cpus 1 --name loadbalancer
-multipass launch --cloud-init worker-cloud-config.yml --disk 5G --mem 1G --cpus 1 --name worker-1
-multipass launch --cloud-init worker-cloud-config.yml --disk 5G --mem 1G --cpus 1 --name worker-2
-echo ""
+$ErrorActionPreference = "Stop"
 
-echo "Copying netplan yaml"
-multipass transfer 01-controller-1-network.yaml controller-1:01-controller-1-network.yaml
-multipass transfer 01-controller-2-network.yaml controller-2:01-controller-2-network.yaml
-multipass transfer 01-loadbalancer-network.yaml loadbalancer:01-loadbalancer-network.yaml
-multipass transfer 01-worker-1-network.yaml worker-1:01-worker-1-network.yaml
-multipass transfer 01-worker-2-network.yaml worker-2:01-worker-2-network.yaml
-echo ""
+$VMs = @(
+    "controller-1",
+    "controller-2",
+    "loadbalancer",
+    "worker-1",
+    "worker-2"
+)
 
-echo "Moving netplan yaml to /etc/netplan"
-multipass exec controller-1 -- sudo cp 01-controller-1-network.yaml /etc/netplan/ 
-multipass exec controller-2 -- sudo cp 01-controller-2-network.yaml /etc/netplan/ 
-multipass exec loadbalancer -- sudo cp 01-loadbalancer-network.yaml /etc/netplan/ 
-multipass exec worker-1 -- sudo cp 01-worker-1-network.yaml /etc/netplan/ 
+Write-Host "Starting VMs"
+
+multipass launch --cloud-init controller-cloud-config.yml --disk 5G --memory 1G --cpus 1 --name controller-1
+Start-Sleep -Seconds 20
+
+multipass launch --cloud-init controller-cloud-config.yml --disk 5G --memory 1G --cpus 1 --name controller-2
+Start-Sleep -Seconds 20
+
+multipass launch --cloud-init controller-cloud-config.yml --disk 5G --memory 1G --cpus 1 --name loadbalancer
+Start-Sleep -Seconds 20
+
+multipass launch --cloud-init worker-cloud-config.yml --disk 5G --memory 1G --cpus 1 --name worker-1
+Start-Sleep -Seconds 20
+
+multipass launch --cloud-init worker-cloud-config.yml --disk 5G --memory 1G --cpus 1 --name worker-2
+Start-Sleep -Seconds 30
+
+Write-Host "Copying netplan yaml"
+
+multipass transfer 01-controller-1-network.yaml controller-1`:01-controller-1-network.yaml
+multipass transfer 01-controller-2-network.yaml controller-2`:01-controller-2-network.yaml
+multipass transfer 01-loadbalancer-network.yaml loadbalancer`:01-loadbalancer-network.yaml
+multipass transfer 01-worker-1-network.yaml worker-1`:01-worker-1-network.yaml
+multipass transfer 01-worker-2-network.yaml worker-2`:01-worker-2-network.yaml
+
+Write-Host "Moving netplan yaml"
+
+multipass exec controller-1 -- sudo cp 01-controller-1-network.yaml /etc/netplan/
+multipass exec controller-2 -- sudo cp 01-controller-2-network.yaml /etc/netplan/
+multipass exec loadbalancer -- sudo cp 01-loadbalancer-network.yaml /etc/netplan/
+multipass exec worker-1 -- sudo cp 01-worker-1-network.yaml /etc/netplan/
 multipass exec worker-2 -- sudo cp 01-worker-2-network.yaml /etc/netplan/
-echo ""
 
-echo "Restarting all hosts to apply netplan"
-multipass restart controller-1
-multipass restart controller-2
-multipass restart loadbalancer
-multipass restart worker-1
-multipass restart worker-2
-echo ""
+Write-Host "Applying netplan"
 
-echo "Completed"
+foreach ($vm in $VMs) {
+    Write-Host "Applying netplan on $vm"
+    multipass exec $vm -- sudo netplan apply
+    Start-Sleep -Seconds 10
+}
+
+Write-Host "Completed"
+
+multipass list
